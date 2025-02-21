a4b0c3c9fe1b9515881665f1534f734518813346
luca_jiang <luca_jiang@netrrix.com>
2025-02-21 14:21:53 +0800

    Add processing logic for user input to start or exit game.

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,6 @@
 #include<string>
 #include<iostream>
-#include <iomanip>
+#include<iomanip>
 
 const int SIZE = 4;
 const int CELL_WIDTH = 5; 
@@ -15,6 +15,20 @@ inline void Message(std::string& str, bool lineFeed = true)
 {
     Message(str.c_str(), lineFeed);
 }
+
+inline void PrintCover()
+{
+    Message("===================================");
+    Message("|         WELCOME TO GAME         |");
+    Message("|    _       __     ___    _ _    |");
+    Message("|   / |     /  \\   |_  )  | | |   |");
+    Message("|   | |    | () |   / /   |_  _|  |");
+    Message("|  _|_|_   _\\__/   /___|   _|_|_  |");
+    Message("|_|\"\"\"\"\"|_|\"\"\"\"\"|_|\"\"\"\"\"|_|\"\"\"\"\"| |");
+    Message("|\"`-0-0-\'\"`-0-0-\'\"`-0-0-\'\"`-0-0-\' |");
+    Message("===================================");
+}
+
 
 inline void PrintChessboardBorder()
 {
@@ -51,23 +65,45 @@ inline void PrintChessboard() {
     }
 }
 
-inline void PrintCover()
+inline void AskStart()
 {
-    Message("===================================");
-    Message("|         WELCOME TO GAME         |");
-    Message("|    _       __     ___    _ _    |");
-    Message("|   / |     /  \\   |_  )  | | |   |");
-    Message("|   | |    | () |   / /   |_  _|  |");
-    Message("|  _|_|_   _\\__/   /___|   _|_|_  |");
-    Message("|_|\"\"\"\"\"|_|\"\"\"\"\"|_|\"\"\"\"\"|_|\"\"\"\"\"| |");
-    Message("|\"`-0-0-\'\"`-0-0-\'\"`-0-0-\'\"`-0-0-\' |");
-    Message("===================================");
+    Message("Please select:");
+    Message("[1] Start Game    [2] Exit ");
+    std::string input;
+    
+    do{
+        try{
+            std::cin >> input;
+            int key = stoi(input);
+            //if 1
+            if(1 == key)
+            {
+                //play
+                Message("game start!");
+                exit(0);
+            }
+            //if 2
+            else if(2 == key)
+            {
+                Message("game exit!");
+                exit(0);
+            }
+            else
+                throw std::runtime_error("");
+        }catch(const std::exception& e){
+            Message("Invalid Input! Please enter 1 or 2:");
+        }
+
+    }while(true);
+
 }
 
 int main()
 {
-    
     PrintCover();
-    PrintChessboard();
+    //init game
+    //AskStart
+    AskStart();
+    
     return 0;
 }
