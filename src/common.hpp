f0b8e8cae979cbe61828a96ae30811dfd8530699
youpJiang <youpjiang@qq.com>
2025-02-25 15:26:04 +0800

    Split Message func to common.hpp

---
+++ src/common.hpp
@@ -0,0 +1,12 @@
+#include<iostream>
+inline void Message(const char* str = "", bool lineFeed = true)
+{
+    std::cout << str;
+    if(lineFeed)
+         std::cout << std::endl;
+}
+
+inline void Message(std::string& str, bool lineFeed = true)
+{
+    Message(str.c_str(), lineFeed);
+}
--- src/main.cpp
+++ src/main.cpp
@@ -1,20 +1,11 @@
 #include<string>
-#include<iostream>
 #include<iomanip>
+
+#include "common.hpp"
 
 const int SIZE = 4;
 const int CELL_WIDTH = 5; 
 
-inline void Message(const char* str = "", bool lineFeed = true)
-{
-    std::cout << str;
-    if(lineFeed)
-         std::cout << std::endl;
-}
-inline void Message(std::string& str, bool lineFeed = true)
-{
-    Message(str.c_str(), lineFeed);
-}
 
 inline void PrintCover()
 {
