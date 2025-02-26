0a81a2bdbce3c9b20aba3be1028759efde5e34ce
youpJiang <youpjiang@qq.com>
2025-02-26 15:40:42 +0800

    Initially add function Up. Attention, still debug mode.

--- src/scene.hpp
+++ src/scene.hpp
@@ -9,13 +9,23 @@
 const int g_SIZE = 4;
 const int g_CELL_WIDTH = 5; 
 
+inline void PrintChessboardBorder();
+void Merge(std::vector<int>& vec);
 
 class ChessBoard
 {
 private:
     std::vector<std::vector<int>> _chessBoard;
+    bool _isWin;
 
-    inline void PrintChessboard();
+    void PrintChessboard() const;
+    void Show();
+
+    void Up();
+    void Down();
+    void Left();
+    void Right();
+    void Merge(std::vector<int>& vec);
 
 public:
     ChessBoard();
@@ -36,7 +46,7 @@ inline void PrintChessboardBorder()
     Message("");
 }
 
-inline void ChessBoard::PrintChessboard() {
+void ChessBoard::PrintChessboard() const{
     PrintChessboardBorder();
 
     // print the whole chessborad.
@@ -63,21 +73,102 @@ inline void ChessBoard::PrintChessboard() {
     }
 }
 
-inline ChessBoard::ChessBoard()
+ChessBoard::ChessBoard()
 {
-    _chessBoard.resize(g_SIZE, std::vector<int>(g_SIZE,0));
+    _isWin = false;
+    _chessBoard.resize(g_SIZE, std::vector<int>(g_SIZE,1));
+}
+
+void ChessBoard::Show()
+{
+    // system("clear");
+    PrintChessboard();
+    Up();
+    PrintChessboard();
+    Up();
+    PrintChessboard();
+    Up();
+    PrintChessboard();
+    Up();
+    PrintChessboard();
 }
 
 void ChessBoard::Play()
 {
-    // for (const auto& row : _chessBoard) {
-    //     for (int val : row) {
-    //         std::cout << val << " ";
-    //     }
-    //     std::cout << std::endl;
-    // }
-    system("clear");
-    PrintChessboard();
+    Show();
+
 }
+
+void ChessBoard::Merge(std::vector<int>& vec)
+{
+    // Message("Before:");
+    // for(auto &it:vec)
+    // {
+    //     std::cout << it <<' ';
+    // }
+    // std::cout << std::endl;
+
+
+    int paddingCount = 0;
+    for(auto it = vec.begin(); it != vec.end();)
+    {
+        //delete 0,count++
+        if(0 == *it)
+        {
+            it = vec.erase(it);
+            paddingCount++;
+        }
+        //vec[i] == vec[i+1]: delete & double. count++
+        else if(it+1 != vec.end() && *it == *(it+1))
+        {
+            *it *= 2;
+            if(1024 == *it)
+                _isWin = true;
+            it = vec.erase(it+1);
+            paddingCount++;
+        }
+        else
+        {
+            ++it;
+        }
+    }
+    //padding 0
+    for(int i = 0; i < paddingCount; ++i)
+    {
+        vec.push_back(0);
+    }
+    
+    // Message("After:");
+    // for(auto &it:vec)
+    // {
+    //     std::cout << it <<' ';
+    // }
+    // std::cout << std::endl;
+}
+
+void ChessBoard::Up()
+{
+    //traverse every column
+    for(int col = 0; col < g_SIZE; ++col)
+    {
+        //to vector
+        std::vector<int> colVec;
+        for(int row = 0 ; row < g_SIZE; ++row)
+        {
+            colVec.push_back(_chessBoard[row][col]);
+        }
+        //call Merge
+        Merge(colVec);
+        //fill back
+        for(int row = 0 ; row < g_SIZE; ++row)
+        {
+            _chessBoard[row][col] = colVec[row];
+        }
+    }
+}
+
+
+
+
 
 #endif // SCENE_HPP
